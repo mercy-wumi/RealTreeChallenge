b74b851eb0af089d383ec50d505f14487a1b2af6
mercy-wumi <m.adams1909@gmail.com>
2022-01-07 00:34:52 +0100

    realtree challenge

---
+++ script.js
@@ -0,0 +1,12 @@
+// using vanilla javascript
+
+// let container = document.querySelector('#container')
+// container.style.textAlign = 'center'
+// let img = document.querySelector('img')
+// img.classList.add('img')
+
+
+// using jQuery
+
+$('#container').css('text-align', 'center');
+$('img').addClass('img');
